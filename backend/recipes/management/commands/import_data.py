7384c847b903a3cafaecbd6070b8c2e7cddc5688
Евгения Клементьева <klemeu@yandex.ru>
2022-11-24 23:48:19 +0300

    Change 30

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -1,14 +1,16 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from recipes.models import Ingredient
 
-INGREDIENT_FILE = 'home/admin/data/ingredients.csv'
+
+STATIC_DATA = settings.BASE_DIR
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        with open(INGREDIENT_FILE) as f:
+        with open(f'{STATIC_DATA}/data/ingredients.csv') as f:
             reader = csv.reader(f)
             next(reader, None)
             ingredient_list = [
