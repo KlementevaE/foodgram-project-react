7d1da8c952a1f5af9f22350e3fcb083613b69af5
Евгения Клементьева <klemeu@yandex.ru>
2022-11-25 01:22:18 +0300

    Change 34

--- backend/api/views.py
+++ backend/api/views.py
@@ -81,6 +81,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    pagination_class = None
     filter_backends = (filters.SearchFilter,)
     search_fields = ('^name',)
 
@@ -90,6 +91,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    pagination_class = None
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
