4acf49c3d7d3d42a0573e2214944ccd87a6a5cd8
Евгения Клементьева <klemeu@yandex.ru>
2022-11-26 12:19:56 +0300

    Change 68

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -162,7 +162,6 @@ class Cart(models.Model):
         User,
         verbose_name=_('user'),
         on_delete=models.CASCADE,
-        related_name='cart',
     )
     recipe = models.ForeignKey(
         Recipe,
